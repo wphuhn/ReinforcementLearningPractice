4917ace65101ce4d2c90bad784379165a961565c
William Huhn <william.paul.huhn@gmail.com>
2020-02-15 16:32:52 -0500

    Move boilerplate functionality into utilities

--- MsPacman_OpenAI/random/random_policy.py
+++ MsPacman_OpenAI/random/random_policy.py
@@ -4,7 +4,8 @@ import gym
 from gym import wrappers
 
 from rl_functions.policies import next_action_with_random_policy
-from rl_functions.utilities import print_run_summary
+from rl_functions.utilities import print_run_summary, make_envs, close_envs, \
+     print_step_statistics
 
 # Roughly 30 minutes runtime on my laptop
 
@@ -18,12 +19,7 @@ OUTPUT_FOLDER = "./videos/" + str(time()) + "/"
 def main():
     start_time = time()
     for run_index in range(NUM_RUNS):
-        env_raw = gym.make(ENV_NAME)
-        if OUTPUT_MOVIE:
-            env = wrappers.Monitor(env_raw, OUTPUT_FOLDER, force=True)
-        else:
-            env = env_raw
-        env.reset()
+        env, env_raw = make_envs(ENV_NAME, output_movie=OUTPUT_MOVIE, output_folder=OUTPUT_FOLDER)
 
         cumul_reward = 0.0
         action = None
@@ -34,18 +30,17 @@ def main():
 
             action = next_action_with_random_policy(env)
             _, reward, done, info = env.step(action)
-
             cumul_reward += reward
+
             if OUTPUT_MOVIE:
-                print("Step {} , Current reward {} , Cummul Reward {} , Lives Left {}".format(timestep + 1, reward, cumul_reward, info["ale.lives"]))
+                print_step_statistics(timestep, reward, cumul_reward, info)
             if done:
                 elapsed_time = time() - start_time
                 print_run_summary(elapsed_time, run_index + 1, timestep + 1, cumul_reward)
                 break
 
-        env.close()
+        close_envs(env, env_raw)
         if OUTPUT_MOVIE:
-            env_raw.close()
             break
 
 if __name__ == "__main__":
--- rl_functions/utilities.py
+++ rl_functions/utilities.py
@@ -1,2 +1,23 @@
+import gym
+from gym import wrappers
+
 def print_run_summary(elapsed_time, run_index, n_steps, cumul_reward):
     print("Elapsed Time: {} , Run: {} , Number Steps: {} , Final Score: {}".format(elapsed_time, run_index, n_steps, cumul_reward))
+
+def print_step_statistics(timestep, reward, cumul_reward, info):
+    print("Step {} , Current reward {} , Cummul Reward {} , Lives Left {}".format(timestep + 1, reward, cumul_reward, info["ale.lives"]))
+
+def make_envs(env_name, output_movie=False, output_folder=None):
+    if output_movie:
+        env_raw = gym.make(env_name)
+        env = wrappers.Monitor(env_raw, output_folder, force=True)
+    else:
+        env_raw = None
+        env = gym.make(env_name)
+    env.reset()
+    return env, env_raw
+
+def close_envs(env, env_raw):
+    env.close()
+    if env_raw is not None:
+        env_raw.close()
