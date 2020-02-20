aa8ffc70fcc4fdf57537e2209b388d5ee5fd5fb3
William Huhn <william.paul.huhn@gmail.com>
2020-02-19 22:34:20 -0500

    Created a policy to return a constant answer
    This replaces the "epsilon-random" policy, which is now known as the
    "epsilon-constant" policy (which better describes what was being done
    in the first place.)

--- rl_functions/policies.py
+++ rl_functions/policies.py
@@ -14,24 +14,36 @@ class RandomPolicy:
         else:
             return self._random_generator.integers(0, self._num_choices)
 
+class ConstantPolicy:
+    def __init__(self, action, epsilon=0.0, random_policy=None, random_generator=None):
+        self.action = action
+        if epsilon > 0.0 and random_policy is None:
+            raise Exception("when specifying an epsilon value greater than 0, you must also provide a random policy!")
+        self._epsilon = epsilon
+        self._random_policy = random_policy
+        self._random_generator = random_generator
+
+    def next_action(self):
+        if self._epsilon == 0.0:
+            return self.action
+
+        random_action = self._random_policy.next_action()
+        if self._epsilon >= 1.0:
+            return random_action
+
+        if self._random_generator is None:
+            random_val = random()
+        else:
+            random_val = self._random_generator.random()
+        if self.action is None or random_val < self._epsilon:
+            self.action = random_action
+        return self.action
+
 def next_action_with_random_policy(num_choices, random_generator=None):
     if random_generator is None:
         return randint(0, num_choices)
     else:
         return random_generator.integers(0, num_choices)
-
-def next_action_with_epsilon_random_policy(num_choices, epsilon, previous_action=None, random_generator=None):
-    random_action = next_action_with_random_policy(num_choices, random_generator=random_generator)
-    if epsilon >= 1.0:
-        return random_action
-
-    if random_generator is None:
-        random_val = random()
-    else:
-        random_val = random_generator.random()
-    if previous_action is None or random_val < epsilon:
-        return random_action
-    return copy.deepcopy(previous_action)
 
 def next_action_with_greedy_policy(q_function, random_generator=None):
     max_indices = np.where(q_function == q_function.max())[0]
--- tests/test_policies.py
+++ tests/test_policies.py
@@ -5,7 +5,7 @@ import numpy as np
 from numpy.random import default_rng
 import pytest
 
-from rl_functions.policies import next_action_with_epsilon_random_policy, next_action_with_greedy_policy, \
+from rl_functions.policies import ConstantPolicy, next_action_with_greedy_policy, \
     next_action_with_epsilon_greedy_policy
 
 from constants import ENV_NAME, N_CHOICES
@@ -29,40 +29,64 @@ def test_two_identical_random_policies_in_parallel_give_the_same_results(random_
         action_copy = random_policy_copy.next_action()
         assert(action_orig == action_copy)
 
-def test_seeded_epsilon_random_policy_gives_expected_results():
-    actions_expected = [6, 5, 5, 5, 5]
-    random_generator = default_rng(seed=0)
-    action_actual = None
-    for action_expected in actions_expected:
-        action_actual = next_action_with_epsilon_random_policy(N_CHOICES, 0.5, action_actual, random_generator=random_generator)
+def test_constant_policy_outcome_doesnt_change():
+    action_expected = 3
+    constant_policy = ConstantPolicy(action_expected)
+    for _ in range(100):
+        action_actual = constant_policy.next_action()
         assert(action_expected == action_actual)
 
-def test_two_epsilon_random_policies_with_random_generators_in_parallel_give_the_same_results():
-    action_1 = None
-    action_2 = None
-    random_generator_1 = default_rng(seed=0)
-    random_generator_2 = default_rng(seed=0)
+def test_epsilon_constant_policy_outcome_doesnt_change_when_epsilon_is_zero():
+    action_expected = 3
+    constant_policy = ConstantPolicy(action_expected, epsilon=0.0)
     for _ in range(100):
-        action_1 = next_action_with_epsilon_random_policy(N_CHOICES, 0.5, action_1, random_generator=random_generator_1)
-        action_2 = next_action_with_epsilon_random_policy(N_CHOICES, 0.5, action_2, random_generator=random_generator_2)
-        assert(action_1 == action_2)
-
-def test_epsilon_random_policy_outcome_doesnt_change_when_epsilon_equals_zero():
-    # Let it run once to pick the first step
-    random_generator = default_rng(seed=0)
-    action_expected = next_action_with_epsilon_random_policy(N_CHOICES, 0.0, None, random_generator=random_generator)
-    action_actual = copy.deepcopy(action_expected)
-    for _ in range(100):
-        action_actual = next_action_with_epsilon_random_policy(N_CHOICES, 0.0, action_actual, random_generator=random_generator)
+        action_actual = constant_policy.next_action()
         assert(action_expected == action_actual)
 
-def test_random_policy_gives_identical_results_to_epsilon_random_policy_when_epsilon_equals_one(random_policy):
-    random_generator_2 = default_rng(seed=0)
-    action_actual = None
+def test_creating_constant_policy_throws_exception_when_epsilon_is_greater_than_one_and_random_policy_not_provided():
+    dummy_action = 3
+    with pytest.raises(Exception) as excinfo:
+        _ = ConstantPolicy(dummy_action, epsilon=1.0)
+    assert "when specifying an epsilon value greater than 0, you must also provide a random policy!" in str(excinfo.value)
+
+def test_random_policy_gives_identical_results_to_epsilon_constant_policy_when_epsilon_equals_one_and_same_rng_used():
+    # Not the best test in the world, I know, but it's an important edge case
+    # We generate an action from a random policy
+    random_policy = create_random_policy_with_fixed_rng(N_CHOICES, 0)
+    action_expected = random_policy.next_action()
+    # Then we copy the state of the random policy *after* the action has been taken
+    random_policy_copy = copy.deepcopy(random_policy)
+    # We use the action and random policy copy to create a new epsilon-constant policy, which *should* be just
+    # the same random policy based on the way we've set it up
+    constant_policy = ConstantPolicy(action_expected, epsilon=1.0, random_policy=random_policy_copy)
+    # And the two should give identical results
     for _ in range(100):
         action_expected = random_policy.next_action()
-        action_actual = next_action_with_epsilon_random_policy(N_CHOICES, 1.0, action_actual, random_generator=random_generator_2)
+        action_actual = constant_policy.next_action()
         assert(action_expected == action_actual)
+
+def test_seeded_epsilon_random_policy_gives_expected_results():
+    initial_action = 3
+    random_policy = create_random_policy_with_fixed_rng(N_CHOICES, 0)
+    random_generator = default_rng(seed=0)
+    constant_policy = ConstantPolicy(initial_action, epsilon=0.5, random_policy=random_policy, random_generator=random_generator)
+    actions_expected = [3, 5, 4, 2, 2]
+    for action_expected in actions_expected:
+        action_actual = constant_policy.next_action()
+        assert(action_expected == action_actual)
+
+def test_two_epsilon_constant_policies_with_random_generators_in_parallel_give_the_same_results():
+    initial_action = 3
+    random_policy_1 = create_random_policy_with_fixed_rng(N_CHOICES, 0)
+    random_policy_2 = create_random_policy_with_fixed_rng(N_CHOICES, 0)
+    random_generator_1 = default_rng(seed=0)
+    random_generator_2 = default_rng(seed=0)
+    constant_policy_1 = ConstantPolicy(initial_action, epsilon=0.5, random_policy=random_policy_1, random_generator=random_generator_1)
+    constant_policy_2 = ConstantPolicy(initial_action, epsilon=0.5, random_policy=random_policy_2, random_generator=random_generator_2)
+    for _ in range(100):
+        action_1 = constant_policy_1.next_action()
+        action_2 = constant_policy_2.next_action()
+        assert(action_1 == action_2)
 
 def test_greedy_policy_gives_expected_results():
     q_function = np.array([17.1, -5.5, 20.2, -20.9, 5.1, 1.0, -1.0, 1.5])
