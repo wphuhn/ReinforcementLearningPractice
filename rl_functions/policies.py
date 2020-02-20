539e2692e94d704b8129d69763c6f96a0e371da3
William Huhn <william.paul.huhn@gmail.com>
2020-02-19 23:21:57 -0500

    Unite epsilon-greedy and greedy into a class
    I have a strong suspicion that the epsilon-ness of Constant and Greedy
    can be inherited from an EpsilonPolicy base class, but I'm going to wait
    until after I've implemented another policy to be sure.

--- rl_functions/policies.py
+++ rl_functions/policies.py
@@ -35,35 +35,40 @@ class ConstantPolicy:
             random_val = random()
         else:
             random_val = self._random_generator.random()
-        if self.action is None or random_val < self._epsilon:
+        if random_val < self._epsilon:
             self.action = random_action
         return self.action
 
-def next_action_with_random_policy(num_choices, random_generator=None):
-    if random_generator is None:
-        return randint(0, num_choices)
-    else:
-        return random_generator.integers(0, num_choices)
+class GreedyPolicy:
+    def __init__(self, epsilon=0.0, random_policy=None, random_generator=None):
+        if epsilon > 0.0 and random_policy is None:
+            raise Exception("when specifying an epsilon value greater than 0, you must also provide a random policy!")
+        self._epsilon = epsilon
+        self._random_policy = random_policy
+        self._random_generator = random_generator
 
-def next_action_with_greedy_policy(q_function, random_generator=None):
-    max_indices = np.where(q_function == q_function.max())[0]
-    if len(max_indices) == 1:
-        return max_indices[0]
-    if random_generator is None:
-        return choice(max_indices)
-    else:
-        return random_generator.choice(max_indices)
+    def _greedy_action(self, q_function):
+        max_indices = np.where(q_function == q_function.max())[0]
+        if len(max_indices) == 1:
+            return max_indices[0]
+        if self._random_generator is None:
+            return choice(max_indices)
+        else:
+            return self._random_generator.choice(max_indices)
 
-def next_action_with_epsilon_greedy_policy(q_function, epsilon, num_choices, random_generator=None):
-    random_action = next_action_with_random_policy(num_choices, random_generator=random_generator)
-    if epsilon >= 1.0:
-        return random_action
+    def next_action(self, q_function):
+        greedy_action = self._greedy_action(q_function)
+        if self._epsilon == 0.0:
+            return greedy_action
 
-    if random_generator is None:
-        random_val = random()
-    else:
-        random_val = random_generator.random()
-    if random_val < epsilon:
-        return random_action
+        random_action = self._random_policy.next_action()
+        if self._epsilon >= 1.0:
+            return random_action
 
-    return next_action_with_greedy_policy(q_function, random_generator=random_generator)
+        if self._random_generator is None:
+            random_val = random()
+        else:
+            random_val = self._random_generator.random()
+        if random_val < self._epsilon:
+            return random_action
+        return greedy_action
--- tests/test_policies.py
+++ tests/test_policies.py
@@ -5,8 +5,7 @@ import numpy as np
 from numpy.random import default_rng
 import pytest
 
-from rl_functions.policies import ConstantPolicy, next_action_with_greedy_policy, \
-    next_action_with_epsilon_greedy_policy
+from rl_functions.policies import ConstantPolicy, GreedyPolicy
 
 from constants import ENV_NAME, N_CHOICES
 from utilities import create_random_policy_with_fixed_rng
@@ -16,8 +15,8 @@ def random_policy():
     return create_random_policy_with_fixed_rng(N_CHOICES, 0)
 
 def test_seeded_random_policy_gives_expected_results(random_policy):
-    actions_expected = [6, 5, 4, 2, 2]
     random_generator = default_rng(seed=0)
+    actions_expected = [6, 5, 4, 2, 2]
     for action_expected in actions_expected:
         action_actual = random_policy.next_action()
         assert(action_expected == action_actual)
@@ -43,13 +42,13 @@ def test_epsilon_constant_policy_outcome_doesnt_change_when_epsilon_is_zero():
         action_actual = constant_policy.next_action()
         assert(action_expected == action_actual)
 
-def test_creating_constant_policy_throws_exception_when_epsilon_is_greater_than_one_and_random_policy_not_provided():
+def test_creating_constant_policy_throws_exception_when_epsilon_is_greater_than_zero_and_random_policy_not_provided():
     dummy_action = 3
     with pytest.raises(Exception) as excinfo:
         _ = ConstantPolicy(dummy_action, epsilon=1.0)
     assert "when specifying an epsilon value greater than 0, you must also provide a random policy!" in str(excinfo.value)
 
-def test_random_policy_gives_identical_results_to_epsilon_constant_policy_when_epsilon_equals_one_and_same_rng_used():
+def test_epsilon_constant_policy_gives_identical_results_to_random_policy_when_epsilon_equals_one_and_same_rng_used():
     # Not the best test in the world, I know, but it's an important edge case
     # We generate an action from a random policy
     random_policy = create_random_policy_with_fixed_rng(N_CHOICES, 0)
@@ -65,7 +64,7 @@ def test_random_policy_gives_identical_results_to_epsilon_constant_policy_when_e
         action_actual = constant_policy.next_action()
         assert(action_expected == action_actual)
 
-def test_seeded_epsilon_random_policy_gives_expected_results():
+def test_seeded_epsilon_constant_policy_gives_expected_results():
     initial_action = 3
     random_policy = create_random_policy_with_fixed_rng(N_CHOICES, 0)
     random_generator = default_rng(seed=0)
@@ -78,9 +77,9 @@ def test_seeded_epsilon_random_policy_gives_expected_results():
 def test_two_epsilon_constant_policies_with_random_generators_in_parallel_give_the_same_results():
     initial_action = 3
     random_policy_1 = create_random_policy_with_fixed_rng(N_CHOICES, 0)
-    random_policy_2 = create_random_policy_with_fixed_rng(N_CHOICES, 0)
+    random_policy_2 = copy.deepcopy(random_policy_1)
     random_generator_1 = default_rng(seed=0)
-    random_generator_2 = default_rng(seed=0)
+    random_generator_2 = copy.deepcopy(random_generator_1)
     constant_policy_1 = ConstantPolicy(initial_action, epsilon=0.5, random_policy=random_policy_1, random_generator=random_generator_1)
     constant_policy_2 = ConstantPolicy(initial_action, epsilon=0.5, random_policy=random_policy_2, random_generator=random_generator_2)
     for _ in range(100):
@@ -88,42 +87,73 @@ def test_two_epsilon_constant_policies_with_random_generators_in_parallel_give_t
         action_2 = constant_policy_2.next_action()
         assert(action_1 == action_2)
 
-def test_greedy_policy_gives_expected_results():
+def test_creating_greedy_policy_throws_exception_when_epsilon_is_greater_than_zero_and_random_policy_not_provided():
+    with pytest.raises(Exception) as excinfo:
+        _ = GreedyPolicy(epsilon=1.0)
+    assert "when specifying an epsilon value greater than 0, you must also provide a random policy!" in str(excinfo.value)
+
+def test_greedy_policy_picks_unique_greedy_answer_when_there_is_a_clear_greedy_maximum():
     q_function = np.array([17.1, -5.5, 20.2, -20.9, 5.1, 1.0, -1.0, 1.5])
+    greedy_policy = GreedyPolicy()
     action_expected = 2
     for _ in range(100):
-        action_actual = next_action_with_greedy_policy(q_function)
+        action_actual = greedy_policy.next_action(q_function)
         assert(action_expected == action_actual)
 
-def test_greedy_policy_breaks_ties_in_a_pseudorandom_fashion():
+def test_epsilon_greedy_policy_picks_unique_greedy_answer_when_there_is_a_unique_greedy_maximum_and_epsilon_is_zero():
+    q_function = np.array([17.1, -5.5, 20.2, -20.9, 5.1, 1.0, -1.0, 1.5])
+    greedy_policy = GreedyPolicy(epsilon=0.0)
+    action_expected = 2
+    for _ in range(100):
+        action_actual = greedy_policy.next_action(q_function)
+        assert(action_expected == action_actual)
+
+def test_greedy_policy_breaks_ties_in_a_pseudorandom_fashion_when_there_is_more_than_one_greedy_maximum():
     q_function = np.array([20, 20, 20, 20, 20, 20, 20, 20])
-    actions_expected = [6, 5, 4, 2, 2]
     random_generator = default_rng(seed=0)
+    greedy_policy = GreedyPolicy(random_generator=random_generator)
+    actions_expected = [6, 5, 4, 2, 2]
     for action_expected in actions_expected:
-        action_actual = next_action_with_greedy_policy(q_function, random_generator=random_generator)
+        action_actual = greedy_policy.next_action(q_function)
         assert(action_expected == action_actual)
 
 def test_epsilon_greedy_policy_gives_expected_results():
-    q_function = np.array([17.1, -5.5, 20.2, -20.9, 5.1, 1.0, -1.0, 1.5])
-    actions_expected = [6, 5, 2, 2, 2]
+    random_policy = create_random_policy_with_fixed_rng(N_CHOICES, 0)
     random_generator = default_rng(seed=0)
+    greedy_policy = GreedyPolicy(epsilon=0.5, random_policy=random_policy, random_generator=random_generator)
+    q_function = np.array([17.1, -5.5, 20.2, -20.9, 5.1, 1.0, -1.0, 1.5])
+    actions_expected = [2, 5, 4, 2, 2, 2]
     for action_expected in actions_expected:
-        action_actual = next_action_with_epsilon_greedy_policy(q_function, 0.5, N_CHOICES, random_generator=random_generator)
+        action_actual = greedy_policy.next_action(q_function)
         assert(action_expected == action_actual)
 
-def test_epsilon_greedy_policy_gives_identical_results_to_greedy_policy_when_epsilon_equals_zero():
+def test_epsilon_greedy_policy_gives_identical_results_to_greedy_policy_when_there_is_a_unique_greedy_maximum_and_epsilon_equals_zero():
     q_function = np.array([17.1, -5.5, 20.2, -20.9, 5.1, 1.0, -1.0, 1.5])
-    random_generator_1 = default_rng(seed=0)
-    random_generator_2 = default_rng(seed=0)
+    random_generator = default_rng(seed=0)
+    greedy_policy = GreedyPolicy()
+    epsilon_greedy_policy = GreedyPolicy(epsilon=0.0)
     for _ in range(100):
-        action_expected = next_action_with_greedy_policy(q_function, random_generator=random_generator_1)
-        action_actual = next_action_with_epsilon_greedy_policy(q_function, 0.0, N_CHOICES, random_generator=random_generator_2)
+        action_expected = greedy_policy.next_action(q_function)
+        action_actual = epsilon_greedy_policy.next_action(q_function)
         assert(action_expected == action_actual)
 
-def test_epsilon_greedy_policy_gives_identical_results_to_random_policy_when_epsilon_equals_one(random_policy):
-    q_function = np.array([17.1, -5.5, 20.2, -20.9, 5.1, 1.0, -1.0, 1.5])
-    random_generator_2 = default_rng(seed=0)
+def test_epsilon_greedy_policy_gives_identical_results_to_greedy_policy_when_there_is_more_than_one_greedy_maximum_and_epsilon_equals_zero():
+    q_function = np.array([20, 20, 20, 20, 20, 20, 20, 20])
+    random_generator_1 = default_rng(seed=0)
+    random_generator_2 = copy.deepcopy(random_generator_1)
+    greedy_policy = GreedyPolicy(random_generator=random_generator_1)
+    epsilon_greedy_policy = GreedyPolicy(epsilon=0.0, random_generator=random_generator_2)
+    for _ in range(100):
+        action_expected = greedy_policy.next_action(q_function)
+        action_actual = epsilon_greedy_policy.next_action(q_function)
+        assert(action_expected == action_actual)
+
+def test_epsilon_greedy_policy_gives_identical_results_to_random_policy_when_epsilon_equals_one_and_same_rng_used():
+    dummy_q_function = np.array([0] * N_CHOICES)
+    random_policy = create_random_policy_with_fixed_rng(N_CHOICES, 0)
+    random_policy_copy = copy.deepcopy(random_policy)
+    greedy_policy = GreedyPolicy(epsilon=1.0, random_policy=random_policy_copy)
     for _ in range(100):
         action_expected = random_policy.next_action()
-        action_actual = next_action_with_epsilon_greedy_policy(q_function, 1.0, N_CHOICES, random_generator=random_generator_2)
+        action_actual = greedy_policy.next_action(dummy_q_function)
         assert(action_expected == action_actual)
