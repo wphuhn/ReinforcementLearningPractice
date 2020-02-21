f6dd7e0ad508e74b237629b0aa1628eec6c92254
William Huhn <william.paul.huhn@gmail.com>
2020-02-20 19:16:49 -0500

    Update MsPacman example
    Examples have been moved to a "trial" folder to reduce the amount of
    clutter in the root directory.

--- trials/MsPacman_OpenAI/epsilon_constant.py
+++ trials/MsPacman_OpenAI/epsilon_constant.py
@@ -3,7 +3,7 @@ from time import time, sleep
 import gym
 from gym import wrappers
 
-from rl_functions.policies import next_action_with_epsilon_random_policy
+from rl_functions.policies import ConstantPolicy, RandomPolicy
 from rl_functions.utilities import run_summary, make_envs, close_envs, \
      step_statistics
 
@@ -14,21 +14,25 @@ MAX_STEPS_PER_RUN = 10000000
 NUM_RUNS = 4200 # Only relevant when OUTPUT_MOVIE = False
 OUTPUT_MOVIE = False
 FRAME_RATE = 1./30. # 30 FPS rendering
-N_ACTIONS = 8 # This should not be hard wired!
 
 def main():
     start_time = time()
+
+    n_actions = gym.make(ENV_NAME).action_space.n
+    random_policy = RandomPolicy(n_actions)
+    action = random_policy.next_action()
+    constant_policy = ConstantPolicy(action, epsilon=EPSILON,
+        random_policy=random_policy)
     for run_index in range(NUM_RUNS):
         env, env_raw = make_envs(ENV_NAME, output_movie=OUTPUT_MOVIE)
 
         cumul_reward = 0.0
-        action = None
         for timestep in range(MAX_STEPS_PER_RUN):
             if OUTPUT_MOVIE:
                 env.render()
                 sleep(FRAME_RATE)
 
-            action = next_action_with_epsilon_random_policy(N_ACTIONS, EPSILON, action)
+            action = constant_policy.next_action()
             _, reward, done, info = env.step(action)
             cumul_reward += reward
 
