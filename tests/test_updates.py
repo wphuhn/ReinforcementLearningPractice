42ddd5abba41376347b254be50faa9e413a2b4c5
William Huhn <william.paul.huhn@gmail.com>
2020-02-27 14:42:50 -0500

    Fix tests for commit 3c21ac03

--- tests/test_updates.py
+++ tests/test_updates.py
@@ -9,7 +9,7 @@ def test_update_q_function_gives_expected_results():
     action = 2
     alpha = 0.5
     reward_action = -4.
-    q_function_expected = np.array([20., 10., 3., 19., 6.])
+    q_function_expected = np.array([20., 10., 0.5, 19., 6.])
     q_function_actual = update_q_function(q_function_prior, action, alpha, reward_action)
     assert(all(q_function_expected == q_function_actual))
 
@@ -21,5 +21,3 @@ def test_update_q_function_does_not_change_prior():
     q_function_expected = copy.deepcopy(q_function_prior)
     _ = update_q_function(q_function_prior, action, alpha, reward_action)
     assert(all(q_function_expected == q_function_prior))
-
-
