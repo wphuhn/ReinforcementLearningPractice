32a102817b69a8b298be8241b8c57c015410610a
William Huhn <william.paul.huhn@gmail.com>
2020-02-29 14:03:26 -0500

    Allow non-existing state-action pairs in q update

--- rl_functions/updates.py
+++ rl_functions/updates.py
@@ -1,8 +1,50 @@
+"""Subroutines for updating non-class objects.
+
+Currently, only the q function update is supported.
+
+    Typical usage example:
+
+    N/A
+"""
+
 import copy
 
-def update_q_function(q_function_prior, state, action, alpha, reward_action):
-    q_function = copy.deepcopy(q_function_prior)
-    q_function[state][action] = q_function[state][action] + alpha * (
-        reward_action - q_function[state][action]
+def update_q_function(q, state, action, alpha, reward):
+    """Update the q function based on reward obtained from a state-action pair.
+
+    The update method used is a simple iterative approach, with scaling factor
+    alpha.  If the state exists in the q-function but the state-action pair does
+    not, the state-action pair will be initialized to the maximum q value for
+    that state before applying the update.  If the state does not exist in the
+    q-function, the state-action pair will be initialized to a value of zero
+    before applying the update.
+
+    Args:
+        q: The q function represented as a two-level nested Iterable, i.e. of
+            form q[state][action].  This argument is not updated in-place.
+        state: The state in the state-action pair whose q value is updated.
+        action: The action in the state-action pair whose q value is updated.
+        alpha: The scaling factor for the update.
+        reward: The reward for the state-action pair whose q value is updated.
+
+    Returns:
+        The updated q function in an identical data format to q.
+
+    Raises:
+        None
+    """
+    q_new = copy.deepcopy(q)
+    # if state does not exists in q, we insert it and the action into the
+    # dictionary with a zero value
+    if state not in q_new:
+       q_new[state] = {}
+       q_new[state][action] = 0.
+    # if state exists in q but the action does not exist, we insert it into the
+    # dictionary with the optimistic value
+    if action not in q_new[state]:
+        max_value = max(q_new[state].values())
+        q_new[state][action] = max_value
+    q_new[state][action] = q_new[state][action] + alpha * (
+        reward - q_new[state][action]
     )
-    return q_function
+    return q_new
--- tests/test_updates.py
+++ tests/test_updates.py
@@ -4,33 +4,93 @@ import numpy as np
 
 from rl_functions.updates import update_q_function
 
-def test_update_q_function_returns_expected_results_when_it_is_called():
+def test_update_q_function_updates_existing_state_action_pair_when_the_pair_is_in_the_q_function():
+    state = 0
+    action = 2
+    old_value = 5.
+    prior = {
+        state: {
+            0: 20.,
+            1: 10.,
+            action: old_value,
+            3: 19.,
+            4: 6.,
+        },
+        1: {
+            0: 25.,
+            1: 15.,
+            2: 0.,
+            3: 14.,
+            4: 11.,
+        },
+    }
+    alpha = 0.5
+    reward = -4.
+    new_value = old_value + alpha * (reward - old_value)
+    expected = copy.deepcopy(prior)
+    expected[state][action] = new_value
+    actual = update_q_function(prior, state, action, alpha, reward)
+    assert expected == actual
+
+def test_update_q_function_adds_new_state_action_pair_when_the_state_is_not_in_the_q_function():
     prior = {
         0: {
             0: 20.,
             1: 10.,
             2: 5.,
             3: 19.,
-            4: 6.
-        }
+            4: 6.,
+        },
+        1.: {
+            0: 25.,
+            1: 15.,
+            2: 0.,
+            3: 14.,
+            4: 11.,
+        },
     }
-    state = 0
-    action = 2
+    state = 2
+    action = 7
     alpha = 0.5
     reward = -4.
-    expected = {
+    new_value = alpha * reward
+    expected = copy.deepcopy(prior)
+    expected[state] = {}
+    expected[state][action] = new_value
+    actual = update_q_function(prior, state, action, alpha, reward)
+    assert expected == actual
+
+def test_update_q_function_adds_new_action_to_existing_state_with_optimistic_value_when_the_action_is_not_associated_with_the_state():
+    state = 1
+    max_value = 25.
+    prior = {
         0: {
             0: 20.,
             1: 10.,
-            2: 0.5,
+            2: 5.,
             3: 19.,
-            4: 6.
-        }
+            4: 6.,
+        },
+        state: {
+            0: max_value,
+            1: 15.,
+            2: 0.,
+            3: 14.,
+            4: 11.,
+        },
     }
+    action = 7
+    alpha = 0.5
+    reward = -4.
+    new_value = max_value + alpha * (reward - max_value)
+    expected = copy.deepcopy(prior)
+    expected[state][action] = new_value
     actual = update_q_function(prior, state, action, alpha, reward)
     assert expected == actual
 
 def test_q_function_does_not_change_when_it_is_supplied_to_update_q_function():
+    state = 0
+    action = 3
     actual = {
         0: {
             0: 20.,
@@ -38,10 +98,15 @@ def test_q_function_does_not_change_when_it_is_supplied_to_update_q_function():
             2: 5.,
             3: 19.,
             4: 6.
-        }
+        },
+        1: {
+            0: 25.,
+            1: 15.,
+            2: 0.,
+            3: 14.,
+            4: 11.,
+        },
     }
-    state = 0
-    action = 2
     alpha = 0.5
     reward = -4.
     expected = copy.deepcopy(actual)
