93a5e04a636c0586570cb5e1e28c9037b1761995
William Huhn <william.paul.huhn@gmail.com>
2020-03-03 20:45:50 -0500

    Generate deterministic policy from q function
    Documentation has been added to the DeterministicPolicy class, and I've
    removed the need to specify the number of states or actions.  This
    really isn't necessary when using a dictionary.

--- rl_functions/policies.py
+++ rl_functions/policies.py
@@ -1,4 +1,17 @@
-import numpy as np
+"""Reinforcement learning policies for predicting actions of agents.
+
+This module contains policies which generate actions based on information that
+the agent has learned about the environment.  Most policies may be made
+epsilon-soft by supplying an epsilon parameter, allowing the agent to
+occasionally go off-policy and return a random action.  Note that when using
+an epsilon-soft policy, you will need to supply a RandomPolicy during
+instantiation to generate random actions.
+
+    Typical usage:
+
+    policy = RandomPolicy(num_actions=5)
+    next_action = policy.next_action()
+"""
 from numpy.random import random, randint, choice
 
 class RandomPolicy(object):
@@ -75,24 +88,79 @@ class GreedyPolicy(object):
         return greedy_action
 
 class DeterministicPolicy(object):
-    def __init__(self, n_states, n_actions):
-        self._n_states = n_states
-        self._n_actions = n_actions
+    """Deterministic policy which outputs a unique action given a state.
+
+    By default, the policy is empty.  State-action pairs may be registered to
+    the policy directly using add_transitions() or may be infered from a
+    q function using generate_greedily_from_q.
+
+    Attributes:
+        None
+    """
+    def __init__(self):
+        """Inits deterministic policy with no registered state-action pairs."""
         self._transitions = {}
 
     def add_transitions(self, trans_dict):
+        """Add state-action pairs to the determinstic policy.
+
+        Args:
+            trans_dict: a dictionary encoding the pairs to add, with the states
+                as keys and the actions as the associate values, i.e. of form
+                {state: action}
+
+        Returns:
+            None
+
+        Raises:
+            Exception when state or action index is negative
+        """
         for state, action in trans_dict.items():
             if state < 0:
                 raise Exception("State index in deterministic policy must be non-negative")
-            if state >= self._n_states:
-                raise Exception(f"State index {state} in deterministic policy is larger than the maximum state index of {self._n_states-1}")
             if action < 0:
                 raise Exception("Action index in deterministic policy must be non-negative")
-            if action >= self._n_actions:
-                raise Exception(f"Action index {action} in deterministic policy is larger than the maximum action index of {self._n_actions-1}")
         self._transitions.update(trans_dict)
 
     def next_action(self, state):
+        """Generate the next action predicted by the policy for a state.
+
+        Args:
+            state: the state whose associated action is being requested
+
+        Returns:
+            The action to perform for the given state
+
+        Raises:
+            Exception when the state supplied by the user has no action
+            associated with it
+        """
         if state not in self._transitions:
             raise Exception(f"State {state} in deterministic policy has no action registered with it")
         return self._transitions[state]
+
+    def generate_greedily_from_q(self, q):
+        """Adds state-action pairs to the based on greedy q-function maximizing.
+
+        If the q function has non-unique maximum values for any state, this
+        method for initializing the deterministic policy cannot be used.
+
+        q: The q function represented as a two-level nested Iterable, i.e. of
+            form q[state][action].
+
+        Returns:
+            None
+
+        Raises:
+            Exception when a state has more than one maximizing action in the
+            q function.
+        """
+        for state in q:
+            q_state = q[state]
+            max_value = max(q_state.values())
+            action = [
+                action for action, value in q_state.items() if (value == max_value)
+            ]
+            if len(action) > 1:
+                raise Exception(f"state {state} does not have a unique greedy action in q function, cannot generate a deterministic policy for it")
+            self.add_transitions({state: action[0]})
--- tests/test_DeterministicPolicy.py
+++ tests/test_DeterministicPolicy.py
@@ -2,72 +2,78 @@ import pytest
 
 from rl_functions.policies import DeterministicPolicy
 
-def test_object_instantiation_is_successful_when_number_of_states_and_actions_are_supplied():
-    n_states = 100
-    n_actions = 50
-    det_policy = DeterministicPolicy(n_states, n_actions)
+@pytest.fixture
+def det_policy():
+    return DeterministicPolicy()
+
+def test_object_instantiation_is_successful_when_no_parameters_are_supplied(det_policy):
     assert isinstance(det_policy, DeterministicPolicy)
 
-def test_policy_throws_exception_when_registering_transition_with_negative_state_index():
-    n_states = 100
-    n_actions = 50
+def test_policy_throws_exception_when_registering_transition_with_negative_state_index(det_policy):
     invalid_state = -1
     valid_action = 25
     invalid_dict = {invalid_state: valid_action}
-    det_policy = DeterministicPolicy(n_states, n_actions)
     with pytest.raises(Exception) as excinfo:
         det_policy.add_transitions(invalid_dict)
     assert "State index in deterministic policy must be non-negative" in str(excinfo.value)
 
-def test_policy_throws_exception_when_registering_transition_with_negative_action_index():
-    n_states = 100
-    n_actions = 50
+def test_policy_throws_exception_when_registering_transition_with_negative_action_index(det_policy):
     valid_state = 50
     invalid_action = -1
     invalid_dict = {valid_state: invalid_action}
-    det_policy = DeterministicPolicy(n_states, n_actions)
     with pytest.raises(Exception) as excinfo:
         det_policy.add_transitions(invalid_dict)
     assert "Action index in deterministic policy must be non-negative" in str(excinfo.value)
 
-def test_policy_throws_exception_when_registering_transition_with_too_high_action_index():
-    n_states = 100
-    n_actions = 50
-    invalid_state = 100
-    valid_action = 25
-    invalid_dict = {invalid_state: valid_action}
-    det_policy = DeterministicPolicy(n_states, n_actions)
-    with pytest.raises(Exception) as excinfo:
-        det_policy.add_transitions(invalid_dict)
-    assert "State index 100 in deterministic policy is larger than the maximum state index of 99" in str(excinfo.value)
-
-def test_policy_throws_exception_when_registering_transition_with_too_high_action_index():
-    n_states = 100
-    n_actions = 50
-    valid_state = 50
-    invalid_action = 50
-    invalid_dict = {valid_state: invalid_action}
-    det_policy = DeterministicPolicy(n_states, n_actions)
-    with pytest.raises(Exception) as excinfo:
-        det_policy.add_transitions(invalid_dict)
-    assert "Action index 50 in deterministic policy is larger than the maximum action index of 49" in str(excinfo.value)
-
-def test_policy_throws_exception_when_predicting_next_action_for_state_that_has_not_been_registered():
-    n_states = 100
-    n_actions = 50
+def test_policy_throws_exception_when_predicting_next_action_for_state_that_has_not_been_registered(det_policy):
     state_with_no_transition = 50
-    det_policy = DeterministicPolicy(n_states, n_actions)
     with pytest.raises(Exception) as excinfo:
         _ = det_policy.next_action(state_with_no_transition)
     assert "State 50 in deterministic policy has no action registered with it" in str(excinfo.value)
 
-def test_policy_provides_next_action_when_state_with_valid_state_action_pair_has_been_registered():
-    n_states = 100
-    n_actions = 50
+def test_policy_provides_next_action_when_state_with_valid_state_action_pair_has_been_registered(det_policy):
     state = 50
     expected = 25
     transitions = {state: expected}
-    det_policy = DeterministicPolicy(n_states, n_actions)
     det_policy.add_transitions(transitions)
     actual = det_policy.next_action(state)
     assert expected == actual
+
+def test_policy_can_be_generated_greedily_from_q_function_when_unique_maximum_exist_for_every_state(det_policy):
+    state_1 = 5
+    state_2 = 15
+    expected_1 = 3
+    expected_2 = 1
+    q = {
+        state_1: {
+            0: 15.,
+            1: 30.,
+            2: 1.,
+            expected_1: 40.,
+        },
+        state_2: {
+            0: 30.,
+            expected_2: 100.,
+            2: -15.,
+            3: 7.,
+            4: 12.,
+        },
+    }
+    det_policy.generate_greedily_from_q(q)
+    actual_1 = det_policy.next_action(state_1)
+    actual_2 = det_policy.next_action(state_2)
+    assert expected_1 == actual_1
+    assert expected_2 == actual_2
+
+def test_policy_throws_exception_when_attempting_to_generate_greedily_from_q_function_but_more_than_one_maximum_exists(det_policy):
+    q = {
+        3: {
+            0: 15.,
+            1: 30.,
+            2: 1.,
+            3: 30.,
+        },
+    }
+    with pytest.raises(Exception) as excinfo:
+        det_policy.generate_greedily_from_q(q)
+    assert "state 3 does not have a unique greedy action in q function, cannot generate a deterministic policy for it" in str(excinfo.value)
