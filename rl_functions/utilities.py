5f91c2e87f4d01ef176d95377c802f93dc3f6d97
William Huhn <william.paul.huhn@gmail.com>
2020-03-15 10:57:35 -0400

    Add Q-learning as an update method

--- rl_functions/utilities.py
+++ rl_functions/utilities.py
@@ -1,9 +1,11 @@
 """Miscellaneous helper subroutines that haven't found a place elsewhere.
 """
+from math import floor
 from time import time
 
 import gym
 from gym import wrappers
+import numpy as np
 
 def run_summary(elapsed_time, episode, n_steps, cumul_reward):
     """Return a string summarizing the current episode.
@@ -109,3 +111,40 @@ def close_envs(env, env_raw=None):
     env.close()
     if env_raw is not None:
         env_raw.close()
+
+class StateEncoder(object):
+    def __init__(self):
+        self._grid = None
+        self._min_values = None
+        self._max_values = None
+        self._factors = None
+
+    def fit(self, grid, min_values, max_values):
+        self._grid = grid[:]
+        self._min_values = min_values[:]
+        self._max_values = max_values[:]
+        # Generate the factors used for the polynomial expansion, i.e. for a
+        # [10, 10, 10] grid the factors would be [1, 10, 100]
+        self._factors = np.array([1], dtype="int")
+        for item in self._grid[:-1]:
+            next_factor = item * self._factors[-1]
+            self._factors = np.append(self._factors, next_factor)
+        return self
+
+    def transform(self, coords):
+        factor = 1
+        transformed = np.array([], dtype="int")
+        for i, value in enumerate(coords):
+            min_value = self._min_values[i]
+            max_value = self._max_values[i]
+            grid = self._grid[i]
+            if value <= min_value:
+                index = 0
+            elif value >= max_value:
+                index = grid - 1
+            else:
+                index = floor(
+                    (grid - 1) * (value - min_value) / (max_value - min_value)
+                )
+            transformed = np.append(transformed, index)
+        return np.dot(self._factors, transformed)
--- tests/test_updates.py
+++ tests/test_updates.py
@@ -7,6 +7,7 @@ from rl_functions.updates import (
     update_iterative,
     update_on_policy_monte_carlo,
     update_sarsa,
+    update_q_learning,
 )
 
 def test_update_iterative_updates_existing_state_action_pair_when_the_pair_is_in_the_q_function():
@@ -471,5 +472,157 @@ def test_sarsa_gives_expected_value_when_parameters_are_valid():
          + alpha * gamma * q[s_p][a_p]
          + (1 - alpha) * q[s][a])
     update_sarsa(trajectory, rewards, q, alpha, gamma)
+    actual = q[s][a]
+    assert expected == actual
+
+def test_q_learning_throws_exception_when_reward_for_current_step_has_already_been_decided():
+    trajectory = [
+        (1, 3),
+        (2,),
+    ]
+    rewards = [0., 1.]
+    q = {
+        1: {3: 10.},
+        2: {0: 20.},
+    }
+    alpha = 0.
+    gamma = 0.5
+    with pytest.raises(Exception) as excinfo:
+        update_q_learning(trajectory, rewards, q, alpha, gamma)
+    assert "Length of trajectory and rewards lists are the same; current state shouldn't yet have a reward when doing Q-learning" in str(excinfo.value)
+
+def test_q_learning_throws_exception_when_previous_state_is_not_in_q():
+    trajectory = [
+        (1, 3),
+        (2,),
+    ]
+    rewards = [0]
+    q = {
+        2: {0: 20.},
+    }
+    alpha = 0.5
+    gamma = 0.5
+    with pytest.raises(Exception) as excinfo:
+        update_q_learning(trajectory, rewards, q, alpha, gamma)
+    assert "previous state 1 not found in q function" in str(excinfo.value)
+
+def test_q_learning_throws_exception_when_previous_action_is_not_in_q():
+    trajectory = [
+        (1, 3),
+        (2,),
+    ]
+    rewards = [4.]
+    q = {
+        1: {},
+        2: {0: 20.},
+    }
+    alpha = 0.5
+    gamma = 0.5
+    with pytest.raises(Exception) as excinfo:
+        update_q_learning(trajectory, rewards, q, alpha, gamma)
+    assert "previous action 3 not found in q function" in str(excinfo.value)
+
+def test_q_learning_throws_exception_when_current_state_is_not_in_q():
+    trajectory = [
+        (1, 3),
+        (2,),
+    ]
+    rewards = [4.]
+    q = {
+        1: {3: 10.},
+    }
+    alpha = 0.5
+    gamma = 0.5
+    with pytest.raises(Exception) as excinfo:
+        update_q_learning(trajectory, rewards, q, alpha, gamma)
+    assert "current state 2 not found in q function" in str(excinfo.value)
+
+def test_q_learning_throws_exception_when_current_state_has_an_associated_action():
+    trajectory = [
+        (1, 3),
+        (2, 0),
+    ]
+    rewards = [4.]
+    q = {
+        1: {3: 10.},
+    }
+    alpha = 0.5
+    gamma = 0.5
+    with pytest.raises(Exception) as excinfo:
+        update_q_learning(trajectory, rewards, q, alpha, gamma)
+    assert "current state shouldn't yet have an action when doing Q-learning" in str(excinfo.value)
+
+def test_q_learning_has_no_effect_on_q_when_alpha_is_zero():
+    trajectory = [
+        (1, 3),
+        (2,),
+    ]
+    rewards = [4.]
+    q = {
+        1: {3: 10.},
+        2: {0: 20.},
+    }
+    alpha = 0.
+    gamma = 0.5
+    expected = copy.deepcopy(q)
+    update_q_learning(trajectory, rewards, q, alpha, gamma)
+    actual = q
+    assert expected == actual
+
+def test_q_learning_replaces_previous_value_with_reward_when_alpha_is_one_and_gamma_is_zero():
+    trajectory = [
+        (1, 3),
+        (2,),
+    ]
+    rewards = [4.]
+    q = {
+        1: {3: 10.},
+        2: {0: 20.},
+    }
+    alpha = 1.
+    gamma = 0.
+    expected = rewards[-1]
+    update_q_learning(trajectory, rewards, q, alpha, gamma)
+    s, a = trajectory[-2]
+    actual = q[s][a]
+    assert expected == actual
+
+###############################################################################
+
+def test_q_learning_replaces_previous_value_with_reward_plus_optimal_value_when_alpha_is_one_and_gamma_is_one():
+    trajectory = [
+        (1, 3),
+        (2,),
+    ]
+    rewards = [4.]
+    q = {
+        1: {3: 10.},
+        2: {0: 20., 7: 300.},
+    }
+    alpha = 1.
+    gamma = 1.
+    expected = rewards[-1] + q[2][7]
+    update_q_learning(trajectory, rewards, q, alpha, gamma)
+    s, a = trajectory[-2]
+    actual = q[s][a]
+    assert expected == actual
+
+def test_q_learning_gives_expected_value_when_parameters_are_valid():
+    trajectory = [
+        (1, 3),
+        (2,),
+    ]
+    rewards = [4.]
+    q = {
+        1: {3: 10.},
+        2: {0: 20., 7: 300.},
+    }
+    alpha = 0.3
+    gamma = 0.2
+    s, a = trajectory[-2]
+    expected = (alpha * rewards[-1]
+         + alpha * gamma * q[2][7]
+         + (1 - alpha) * q[s][a])
+    update_q_learning(trajectory, rewards, q, alpha, gamma)
     actual = q[s][a]
     assert expected == actual
