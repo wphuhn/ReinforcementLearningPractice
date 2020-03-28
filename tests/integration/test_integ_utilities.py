1891b6998fc23ba7bf42fcf30a65bbfdcdbdae62
William Huhn <whuhn@anl.gov>
2020-03-28 13:27:01 -0400

    Split unit and integration tests

--- tests/integration/test_integ_utilities.py
+++ tests/integration/test_integ_utilities.py
@@ -1,21 +1,9 @@
 from rl_functions.utilities import (
-    run_summary,
-    step_statistics,
     make_envs,
     close_envs,
 )
 
 from constants import ENV_NAME
-
-def test_run_summary_outputs_properly_formatted_string():
-    expected = "Episode: 2 , Elapsed Time for Episode: 0.001 s , Num. Steps in Episode: 500 , Cumulative Episode Reward: 9001"
-    actual = run_summary(0.001, 2, 500, 9001)
-    assert actual == expected
-
-def test_step_statistics_outputs_properly_formatted_string():
-    expected = "    Step: 3 , Reward This Step: 17 , Cumulative Reward This Episode: 9002 , Info: 9"
-    actual = step_statistics(3, 17, 9002, 9)
-    assert actual == expected
 
 #TODO: Tests for make_envs
 
